c8a652393e3788c09799bd0d79711a2525e46377
Asim Okby <asemokbey@gmail.com>
2019-10-09 10:14:27 +0300

    Created a task list

--- planner/views.py
+++ planner/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Task
-
+from django.views.generic.edit import CreateView
 
 class IndexView(generic.ListView):
     template_name = "planner/index.html"
@@ -9,3 +9,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Task.objects.all()
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = ["text", "author"]
