9d512bb3fb5a77e95a1ce7de5af2d3243bedd3a9
Asim Okby <asemokbey@gmail.com>
2019-10-10 21:47:53 +0300

    user authentication added

--- planner/urls.py
+++ planner/urls.py
@@ -6,7 +6,9 @@ app_name = "planner"
 urlpatterns = [
     # /planner/
     path("", views.IndexView.as_view(), name="index"),
-    path("/add", views.TaskCreate.as_view(), name="task-add")
-
+    path("add", views.TaskCreate.as_view(), name="task-add"),
+    path("login", views.login_view, name ="login_view"),
+    path('register', views.register_view, name="register_view"),
+    path('logout', views.logout_view)
 
 ]
--- planner/views.py
+++ planner/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Task
 from django.views.generic.edit import CreateView
-
+from django.http import HttpResponse
 class IndexView(generic.ListView):
     template_name = "planner/index.html"
     context_object_name = "task_list"
@@ -13,3 +13,57 @@ class IndexView(generic.ListView):
 class TaskCreate(CreateView):
     model = Task
     fields = ["text", "author"]
+
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import UserLoginForm, UserRegisterForm
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout
+    )
+    
+
+def login_view(request):
+    next = request.GET.get('next')
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {
+        'form':form,
+    }
+    return render(request, 'planner/login.html', context)
+
+
+def register_view(request):
+    next = request.GET.get('next')
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        if next:
+            return redirect(next)
+        return redirect('/')
+
+    context = {
+        'form': form,
+    }
+    return render(request, "planner/signup.html", context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
