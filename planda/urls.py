f7811fe8f20da64f3d24f0f33eb41e3005a2103a
Ibrahim <ibrahim.tigrek@gmail.com>
2019-10-16 12:47:18 +0300

    moved login css to login.css file

--- planda/urls.py
+++ planda/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     
     path('admin', admin.site.urls),
-    path('', include('planner.urls'))
+    path('', include('planner.urls')),
+    # path('/login', include('login.urls'))
+
 
 ]
