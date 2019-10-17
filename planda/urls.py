e741ffb35af356a1fa9681e9b443fa86535f2ac2
Ibrahim <ibrahim.tigrek@gmail.com>
2019-10-17 21:20:02 +0300

    added base url to planner page

--- planda/urls.py
+++ planda/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('', include('planner.urls')),
+    path('planner/', include('planner.urls')),
 
     # /home/
     path('home/', include('homepage.urls')),
--- planner/urls.py
+++ planner/urls.py
@@ -6,10 +6,14 @@ from . import views
 app_name = "planner"
 
 urlpatterns = [
+
+    # /planner/
     path("", views.IndexView.as_view(), name="index"),
 
     # path("add", views.TaskCreate.as_view(), name="task-add"),
+    # /planner/add/task/
     path("add/task/", views.TaskCreate.as_view(), name="add_task"),
+    # /planner/remove/1/
     path("remove/<int:id>", views.remove_task, name="remove-task"),
 
     
