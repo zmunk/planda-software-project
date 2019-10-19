236390171eec57483072ffdc3206ca112554e29f
Ibrahim <ibrahim.tigrek@gmail.com>
2019-10-19 17:41:07 +0300

    redesigned front-end appearance with bootstrap

--- planda/settings.py
+++ planda/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -121,4 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_DIR = os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
--- planner/urls.py
+++ planner/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.urls import path
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 app_name = "planner"
@@ -21,3 +23,5 @@ urlpatterns = [
 
     
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
