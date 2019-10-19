c53294e9984f99b94452c12e10fdb3fa0345100c
Ibrahim <ibrahim.tigrek@gmail.com>
2019-10-19 18:00:10 +0300

    added login and sign up links to home page

--- planda/settings.py
+++ planda/settings.py
@@ -120,12 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_DIR = os.path.join(BASE_DIR,'static')
-# STATICFILES_DIRS = [
-#     STATIC_DIR,
-# ]
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static/'),
-)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+# SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+# STATICFILES_DIRS = (
+#   os.path.join(SITE_ROOT, 'static/'),
+# )
--- planda/urls.py
+++ planda/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # /
@@ -26,3 +28,7 @@ urlpatterns = [
     # /planner/
     path('planner/', include('planner.urls', namespace="planner-namespace")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
