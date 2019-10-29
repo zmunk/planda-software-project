517eade00dbf5b63ce79b487b85a6e17e57a82ce
Ibrahim <ibrahim.tigrek@gmail.com>
2019-10-29 18:42:54 +0300

    added category

--- planner/admin.py
+++ planner/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Task
+from .models import Task, Category
 
 admin.site.register(Task)
-# admin.site.register(Category)
+##
+admin.site.register(Category)
+##
