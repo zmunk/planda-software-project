425a82bf9ff1b3fd8782e5c3edc0ba9b54319a66
Ibrahim <ibrahim.tigrek@gmail.com>
2019-10-29 18:28:12 +0300

    category undo

--- planner/models.py
+++ planner/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from django.urls import reverse
 
 
-class Category(models.Model):
-    category_name = models.CharField(max_length=250)
-
-    def __str__(self):
-        return "Category: {}".format(self.category_name)
-
-    def get_absolute_url(self):
-        # go to album that was just created
-        return reverse("planner-namespace:index")
+# class Category(models.Model):
+#     category_name = models.CharField(max_length=250)
+#
+#     def __str__(self):
+#         return "Category: {}".format(self.category_name)
+#
+#     def get_absolute_url(self):
+#         # go to album that was just created
+#         return reverse("planner-namespace:index")
 
 
 class Task(models.Model):
     text = models.CharField(max_length=1000)
     author = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, default="", on_delete=models.CASCADE)
+    # category = models.ForeignKey(Category, default="", on_delete=models.CASCADE)
 
     def __str__(self):
         return "Task by {}".format(self.author)
