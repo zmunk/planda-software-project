68965c490326f4dc9a1d1ac1dc5bb81b5a6dd397
Ibrahim <ibrahim.tigrek@gmail.com>
2019-11-05 12:54:19 +0300

    fixed conflicts

--- planner/views.py
+++ planner/views.py
@@ -77,10 +77,6 @@ class CategoryCreate(CreateView):
 # ----------- Task
 class TaskCreate(CreateView):
     model = Task
-<<<<<<< HEAD
-=======
-    success_url = reverse_lazy("planner-namespace:project_page")
->>>>>>> 70f741a4d960f876784be1a0df116c9c8cd99865
     fields = ["text"]
 
     def get_success_url(self):
@@ -109,29 +105,23 @@ class TaskCreate(CreateView):
 class TaskDelete(DeleteView):
     template_name = "planner/remove-task.html"
     model = Task
-<<<<<<< HEAD
-    # success_url = reverse_lazy("planner-namespace:project_page")
 
     def get_success_url(self):
         # project_id  = something
         return reverse('planner-namespace:project_page', args=(self.kwargs["project_id"],))
 
-    def get_object(self, **kwargs):
+    def get_project_id(self, **kwargs):
         pk = self.kwargs.get("project_id")
-=======
-    success_url = reverse_lazy("planner-namespace:projects_listed")
+        return pk
+
+    def get_object(self, **kwargs):
+        pk = self.kwargs.get("task_id")
+        return get_object_or_404(Task, id=pk)
 
     def category_id(self):
         # allows html to access project_id through: {{ view.project_id }}
-        pk = self.kwargs.get("category_id") ##TODO
+        pk = self.kwargs.get("category_id") ## TODO
         return get_object_or_404(Category, id=pk)
-
-
-    def get_object(self, **kwargs):
-        pk = self.kwargs.get("pk")
->>>>>>> 70f741a4d960f876784be1a0df116c9c8cd99865
-        return get_object_or_404(Task, id=pk)
-
 
 
 
@@ -147,17 +137,11 @@ class DetailView(generic.DetailView):
 
 class TaskUpdate(UpdateView):
     model = Task
-<<<<<<< HEAD
-    fields = "__all__"
-    # success_url = reverse_lazy("planner-namespace:project_page")
+    fields = ["text"]
 
     def get_success_url(self):
         # project_id  = something
         return reverse('planner-namespace:project_page', args=(self.kwargs["project_id"],))
-=======
-    fields = ["text"]
-    success_url = reverse_lazy("planner-namespace:projects_listed")
->>>>>>> 70f741a4d960f876784be1a0df116c9c8cd99865
 
     def get_object(self, queryset=None):
         pk = self.kwargs.get("pk")
