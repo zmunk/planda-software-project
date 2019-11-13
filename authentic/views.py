7a9f401c067630fbd60a428d306be8d6f87b1992
Ibrahim <ibrahim.tigrek@gmail.com>
2019-11-13 21:34:27 +0300

    fixed login bug

--- authentic/views.py
+++ authentic/views.py
@@ -10,23 +10,6 @@ from django.contrib.auth import (
     logout
     )
 
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-            else:
-                return HttpResponse("Your account was inactive.")
-        else:
-            print("Someone tried to login and failed.")
-            print("They used username: {} and password: {}".format(username,password))
-            return HttpResponse("Invalid login details given")
-    else:
-        return render(request, 'authentic/login.html', {})
 
 def login_view(request):
     next = request.GET.get('next')
