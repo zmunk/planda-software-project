1bd8354cea79152a6dd4052e40fee39b60899df6
Asim Okby <asemokbey@gmail.com>
2019-12-08 00:05:42 +0300

    user redirected to login page after registeration

--- registration/views.py
+++ registration/views.py
@@ -58,7 +58,6 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request, user)
         return redirect('planner:landing_page')
         # return reverse("/")
     else:
