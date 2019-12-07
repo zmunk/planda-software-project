72f5aee9c6010d9f5c392a7763d25cafcbf1b0cc
Ahmad Asaad <a.asaad@windowslive.com>
2019-12-07 17:00:34 +0300

    Sign Up requires Email, Auto-Login after Registration

--- registration/forms.py
+++ registration/forms.py
@@ -5,11 +5,10 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
-    # email = forms.EmailField()
-
+    email = forms.EmailField()
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "password1", "password2"]
         # note: can add email to fields if necessary
 
 
