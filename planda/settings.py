5ae46ff78adce7bfc99c626f14f9b004378894e0
Ahmad Asaad <a.asaad@windowslive.com>
2019-12-07 17:32:39 +0300

    Sign Up requires Email, Auto-Login after Registration

--- planda/settings.py
+++ planda/settings.py
@@ -136,3 +136,12 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'static/'),
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_USE_SLL = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'plandasoftware@gmail.com'
+EMAIL_HOST_PASSWORD = 'ASDFGHJKL99'
+DEFAULT_FROM_EMAIL = 'plandasoftware@gmail.com'
