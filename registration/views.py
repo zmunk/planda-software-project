4fbf375a0088963f38f5baac851f9d12706d6b1b
Ibrahim <ibrahim.tigrek@gmail.com>
2019-12-18 09:34:28 +0300

    form errors

--- registration/views.py
+++ registration/views.py
@@ -1,21 +1,15 @@
-# from django.contrib.auth.models import User
-from .models import User
 from django.contrib.sites.shortcuts import get_current_site
-from django.core.mail import send_mail, EmailMessage
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.contrib.auth import logout
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib import messages
-
-from planda import settings
 from registration.tokens import account_activation_token
 from .forms import RegisterForm
-# from .models import UserProfile # TODO
+from .models import User
 
 
 def register(request):
@@ -26,7 +20,7 @@ def register(request):
 
             if User.objects.filter(username=user.username).exists():
                 messages.error(request, "That username already exists")
-            # if user.mail is in database: display error
+
             if User.objects.filter(email=user.email).exists():
                 messages.error(request, "An account already exists with that email")
 
